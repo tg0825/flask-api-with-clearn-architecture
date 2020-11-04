6a4bc9bcd5dd797bffee469b0202c16b730e0cb0
ted <ted@cupist.com>
2020-11-04 12:46:49 +0900

    [add] api board

--- app/http/api/v1/board.py
+++ app/http/api/v1/board.py
@@ -1,7 +1,7 @@
 from flask import request
 
 from app.http.api import api
-
+from app.http.requests.board.create_board import CreateBoardRequestObject
 
 @api.route("/board")
 def get_board():
@@ -11,9 +11,10 @@ def get_board():
 @api.route("/board", methods=["POST"])
 def create_board():
     # 1. request 데이터를 파싱
+    req = CreateBoardRequestObject.from_dict(a_dict=request.json)
     # 2. 유즈케이스 실행 (dto)
     # 3. 프리젠터 실행
-    return "post board"
+    return req
 
 
 @api.route("/board", methods=["DELETE"])
