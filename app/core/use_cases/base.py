dd54df24a902d527b1929eb568868f8f11d501e2
ted <ted@cupist.com>
2020-11-18 12:22:27 +0900

    base use case 추가

--- app/core/use_cases/base.py
+++ app/core/use_cases/base.py
@@ -1,3 +1,10 @@
+from app.repositories import BoardRepository
+
+
 class BaseUseCase:
-    def __init__(self):
+    def __init__(
+            self,
+            board_repo: BoardRepository
+    ) -> None:
+        self.board_repo = board_repo
         pass
