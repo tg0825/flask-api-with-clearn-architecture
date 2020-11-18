15a1a6eccbc2c00f8c70e91e729c8dba3377c8a9
ted <ted@cupist.com>
2020-11-18 13:56:38 +0900

    [add] create_board 메서드 추가

--- app/repositories/orm/orm_repository.py
+++ app/repositories/orm/orm_repository.py
@@ -1,3 +1,9 @@
 class BoardRepository:
-    def __init__(self):
-        pass
+    def create_board(self, dto: CreateDto = None) -> Board:
+        board = BoardModles(
+
+        )
+
+        session.add(board)
+        session.commit()
+        return board.to_entity()
