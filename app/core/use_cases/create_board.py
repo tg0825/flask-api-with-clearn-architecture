fe0e2427b384bff2e769c844df39f56100ad51ee
ted <ted@cupist.com>
2020-11-25 12:42:47 +0900

    [add] 불필요한 코드 임시 주석

--- app/core/use_cases/create_board.py
+++ app/core/use_cases/create_board.py
@@ -4,7 +4,8 @@ from app.core.use_cases.base import BaseUseCase
 
 class CreateBoardUseCase(BaseUseCase):
     def execute(self, dto=CreateBoardDto):
-        if not dto.text:
-            return
+
+        # if not dto.title:
+        #     return
 
         board = self.board_repo.create_board(dto=dto)
