e0b5e6af760434161c2bbcf4a569b9aa5e62baa4
ted <ted@cupist.com>
2020-11-25 14:22:44 +0900

    [add] 응답값 설정

--- app/core/exceptions.py
+++ app/core/exceptions.py
@@ -4,6 +4,18 @@ from http import HTTPStatus
 class InvalidRequestException(Exception):
     status_code = 400
 
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['detail'] = self.message
+        return rv
+
 
 class NotFoundException(Exception):
     code = HTTPStatus.NOT_FOUND
