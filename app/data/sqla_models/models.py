a26ea63f13c01e3d5c3ff4dd04e66b36640d2c82
ted <ted@cupist.com>
2020-11-25 12:32:28 +0900

    [add] model에서 to_entity 추가

--- app/data/sqla_models/models.py
+++ app/data/sqla_models/models.py
@@ -1,4 +1,6 @@
 from app.extensions.database import db
+from app.core.domain.board import Board
+
 
 class BoardModels(db.Model):
     __tablename__ = 'board'
@@ -10,3 +12,12 @@ class BoardModels(db.Model):
     body = db.Column()
     user_id = db.Column()
     is_deleted = db.Column()
+
+    def to_entity(self):
+        return Board(
+            id=self.id,
+            title=self.title,
+            body=self.body,
+            user_id=self.user_id,
+            is_deleted=self.is_deleted
+        )
