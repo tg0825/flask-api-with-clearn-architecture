c825b61274e8425ce66e9caa080ff511b73ee8e3
ted <ted@cupist.com>
2020-12-23 12:14:27 +0900

    [add] delete repo, dto 추가

--- app/core/dto/board.py
+++ app/core/dto/board.py
@@ -6,3 +6,8 @@ class CreateBoardDto:
     title: str = None
     body: str = None
     user_id: int = None
+
+
+@dataclass
+class DeleteBoardDto:
+    id: int = None
--- app/repositories/orm/orm_repository.py
+++ app/repositories/orm/orm_repository.py
@@ -1,4 +1,4 @@
-from app.core.dto.board import CreateBoardDto
+from app.core.dto.board import CreateBoardDto, DeleteBoardDto
 from app.data.sqla_models.models import BoardModels
 from app.core.domain.board import Board
 
@@ -8,12 +8,13 @@ from app.extensions.database import session
 class BoardRepository:
     def create_board(self, dto: CreateBoardDto = None) -> Board:
         board = BoardModels(
-            title=dto.title,
-            body=dto.body,
-            user_id=dto.user_id,
-            is_deleted=False
+            title=dto.title, body=dto.body, user_id=dto.user_id, is_deleted=False
         )
 
         session.add(board)
         session.commit()
         return board.to_entity()
+
+    def delete_board(self, dto: DeleteBoardDto = None) -> None:
+        session.query(BoardModels).filter(BoardModels.id == dto.id).delete(BoardModels)
+        session.commit()
